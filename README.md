2c23eddb3825a2220330b5d3c83a5f019ee03a25
WOLFIE-OG <oliverjtodd@yahoo.com>
2024-11-28 16:05:39 +0000

    + Update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # CPP-Module-01
+
+This module is designed to help you understand the memory allocation, reference, pointers to members and the usage of the switch in CPP.
+
+## State
+
+[![cpp-module-01 Build](https://img.shields.io/github/actions/workflow/status/WOLFIE-OG/CPP-Module-01/makefile.yml?style=for-the-badge)](https://github.com/WOLFIE-OG/CPP-Module-01/actions/workflows/makefile.yml) ![cpp-module-01 Score](https://img.shields.io/badge/Score-N/A-grey?style=for-the-badge)
+
+## Installation & Building
+
+To install and build
+
+```bash
+  git clone https://github.com/WOLFIE-OG/CPP-Module-01
+  cd CPP-Module-01
+  cd ex00
+  make
+```
+
+## Testing
+
+Tests for each exXX
+
+### ex00
+
+```bash
+  cd ex00
+  ./zombie
+```
---
+++ ex00/include/Zombie.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: otodd <otodd@student.42london.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 15:52:37 by otodd             #+#    #+#             */
+/*   Updated: 2024/11/28 15:56:02 by otodd            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_H
+# define ZOMBIE_H
+# include <iostream>
+
+class Zombie {
+	private:
+		std::string	name;
+		void		announce(void); 
+};
+
+#endif
